332f3738415b9217392d93a3bc44a4f20adc8a7c
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2020-05-23 12:31:58 -0400

    Extracted logic

--- food/src/hooks/useResults.js
+++ food/src/hooks/useResults.js
@@ -2,31 +2,27 @@ import { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
 
 export default () => {
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async searchTerm => {
-    console.log('Hi there!');
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
+    const searchApi = async searchTerm => {
+      try {
+        const response = await yelp.get('/search', {
+          params: {
+            limit: 50,
+            term: searchTerm,
+            location: 'san jose'
+          }
+        });
+        setResults(response.data.businesses);
+      } catch (err) {
+        setErrorMessage('Something went wrong');
+      }
+    };
 
-  // Call searchApi when component
-  // is first rendered.  BAD CODE!
-  // searchApi('pasta');
-  useEffect(() => {
-    searchApi('pasta');
-  }, []);
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
 
-  return [searchApi, results, errorMessage];
+    return [searchApi, results, errorMessage];
 };
