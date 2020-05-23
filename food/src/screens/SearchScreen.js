bcae4e49a5483bd0fa3876fcdecec5e130073c8a
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2020-05-23 12:28:02 -0400

    Commented on extracting hook logic outside the component for code reuse

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -6,10 +6,16 @@ import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [searchApi, results, errorMessage] = useResults();
+  
+  /* **** Some hooks logic has been extracted out of this component and placed in useResults.js ****
+          Basically just a function call that can take in parameters and return things.
+          It will be executed each time this component needs to be rendered (column 1,2 and 3 of the lifecycle diagram)
+          because the functional component is just one big function so things in it execute.
+  */
+  
+  const [searchApi, results, errorMessage] = useResults();                
 
-  const filterResultsByPrice = price => {
-    // price === '$' || '$$' || '$$$'
+  const filterResultsByPrice = price => {                 // price === '$' || '$$' || '$$$'
     return results.filter(result => {
       return result.price === price;
     });
@@ -17,22 +23,12 @@ const SearchScreen = () => {
 
   return (
     <>
-      <SearchBar
-        term={term}
-        onTermChange={setTerm}
-        onTermSubmit={() => searchApi(term)}
-      />
-      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <SearchBar        term={term}        onTermChange={setTerm}        onTermSubmit={() => searchApi(term)} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}               {/* conditional rendering design pattern */}
       <ScrollView>
-        <ResultsList
-          results={filterResultsByPrice('$')}
-          title="Cost Effective"
-        />
-        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-        <ResultsList
-          results={filterResultsByPrice('$$$')}
-          title="Big Spender"
-        />
+            <ResultsList          results={filterResultsByPrice('$')}           title="Cost Effective"        />
+            <ResultsList          results={filterResultsByPrice('$$')}          title="Bit Pricier"           />
+            <ResultsList          results={filterResultsByPrice('$$$')}         title="Big Spender"           />
       </ScrollView>
     </>
   );
